58d900bd2c623d6a063bce2c28c69aded90419f2
a-scollin <andrewscollin@hotmail.co.uk>
2021-11-21 19:04:55 +0000

    Added structure n changed theme

--- src/App.js
+++ src/App.js
@@ -6,26 +6,26 @@ import Header from './Header';
 
 const Home = () => (
   <div>
-    <h2>Welcome to the Cryptozoo!</h2>
+    <h2>Welcome to <h1>Cryptography in the classroom!</h1></h2>
     <div>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
 
     </div>
     <div className='cards'>
          <Card
-          img='https://media.istockphoto.com/photos/koala-resting-and-sleeping-on-his-tree-picture-id530674261?b=1&k=20&m=530674261&s=170667a&w=0&h=HW1-392LqVVgHxIiKqXNUbK9YY727-6EOOzfVqeWSV8='
-          title='Learn About Hash functions'
-          author='Meet Kyle the Koala'
-          linkpath='/home/hash-functions' />
+          img='https://media.istockphoto.com/photos/digital-security-concept-picture-id1289956604?b=1&k=20&m=1289956604&s=170667a&w=0&h=fpkFDlqRrw_IzsQpZkuYqfgCUw7VMemUT8IuCH4-e9w='
+          title='Learn About Security'
+          author='Introduction'
+          linkpath='/home/security' />
           <Card
-          img='https://images.newscientist.com/wp-content/uploads/2016/02/10490-00244-852.jpg'
-          title='Learn About Substitution Ciphers'
-          author='Meet Ceaser the Gorilla' 
-          linkpath='/home/substitution-ciphers'/>
+          img='https://images.immediate.co.uk/production/volatile/sites/4/2020/12/GettyImages-966873284-crop-c414d21.jpg?quality=90&resize=940%2C400'
+          title='Learn About Private Keys'
+          author='Symmetric Encryption' 
+          linkpath='/home/private-keys'/>
           <Card
-          img='https://wallpaperaccess.com/full/1344063.jpg'
-          title='Learn About Public Key Encryption'
-          author='Meet Carrie the Chameleon' 
-          linkpath='/home/public-key-encryption'/>
+          img='https://images.theconversation.com/files/187185/original/file-20170922-15786-1fe7g2a.jpg?ixlib=rb-1.1.0&q=45&auto=format&w=500&h=500.0'
+          title='Learn About Public Key Encryption & Anonymity'
+          author='Asymmetric Encryption & 3DC' 
+          linkpath='/home/public-keys'/>
       </div>  
 
   </div>
@@ -51,34 +51,55 @@ class Card extends React.Component {
 
 const Teachers = () => (
   <div>
-    <h2>Materials for teachers</h2>
-    <div> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</div>
+    <h1>Materials for teachers</h1>
+    <ul>
+    <h2>Section 1 : Security</h2>
+    <br/>
+      <li>Link to the activity sheet</li>
+      <li>Link to the lesson plan</li>
+      <li>Link to the Video</li>
+      <br/>
+      <h2>Section 2 : Private Keys</h2>
+      <br/>
+      <li>Link to the lesson plan</li>
+      <li>Link to the lesson plan</li>
+      <li>Link to the Video</li>
+      <br/>
+      <h2>Section 3 : Public Keys</h2>
+      <br/>
+      <li>Link to the lesson plan</li>
+      <li>Link to the lesson plan</li>
+      <li>Link to the Video</li>
+    </ul>
   </div>
 );
 
 const Contact = () => (
   <div>
     <h2>Contact</h2>
-    <div>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</div>
+    <h1><a href="https://www.andrewscollin.com">Get in touch with me here!</a>
+</h1>
   </div>
 );
 
-const Hash = () => (
+const Security = () => (
   <div>
-    <image src='koala.jpeg'/>
-    <h2>Hash</h2>
+    <h2>Security</h2>
+    <iframe width="560" height="315" src="https://www.youtube.com/embed/cWCg9f-eAH0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
   </div>
 );
 
-const Sub = () => (
+const Priv = () => (
   <div>
-    <h2>Sub</h2>
+    <h2>Private Keys</h2>
+    <iframe width="560" height="315" src="https://www.youtube.com/embed/cWCg9f-eAH0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
   </div>
 );
 
 const Pke = () => (
   <div>
-    <h2>Pke</h2>
+    <h2>Public Keys & Anonymity</h2>
+    <iframe width="560" height="315" src="https://www.youtube.com/embed/cWCg9f-eAH0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
   </div>
 );
 
@@ -94,9 +115,9 @@ function App() {
         <Route exact path='/teachers' component={Teachers} />
         <Route exact path='/contact' component={Contact} />
         <Route path='/home' component={Home} />
-        <Route exact path='/home/hash-functions' component={Hash} />
-        <Route exact path='/home/substitution-ciphers' component={Sub} />
-        <Route exact path='/home/public-key-encryption' component={Pke} />
+        <Route exact path='/home/security' component={Security} />
+        <Route exact path='/home/private-keys' component={Priv} />
+        <Route exact path='/home/public-keys' component={Pke} />
         <Redirect to="/home" />
       </Router>
       
